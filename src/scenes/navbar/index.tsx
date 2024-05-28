90aa82780497dd60211b652c06ea5997ddabc400
Vishesh <vishesh.gubrani@gmail.com>
2024-05-28 11:19:07 +0530

    added navbar component

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
+import Logo from "@/assets/Logo.png";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  const flexBetween = "flex items-center justify-between";
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5`}></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
